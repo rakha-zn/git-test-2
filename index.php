572b17148e2ccc5ce5a5a11375feb712a3d22b0d
rakha-zn <rakhazaidan3113@gmail.com>
2025-08-14 15:05:02 +0700

    Menambahkan list

--- index.php
+++ index.php
@@ -8,5 +8,11 @@
 <body>
     <h1>Hello, World!</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci, sequi delectus culpa vitae facilis quam voluptates rem sapiente labore sunt voluptatum maiores quaerat maxime. Consectetur quis id obcaecati repellendus. Sapiente!</p>
+
+    <ul>
+        <li>1</li>
+        <li>2</li>
+        <li>3</li>
+    </ul>
 </body>
 </html>
